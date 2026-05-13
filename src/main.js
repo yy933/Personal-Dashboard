820553d05220b3488427432146eaa23c388cb659
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-13 15:44:04 +0800

    feat: update background image from Unsplash API. Add crypto curreency info with data fetched from CoinGecko API

--- src/main.js
+++ src/main.js
@@ -1,15 +1,35 @@
-import "./style.css";
-
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.urls.regular);
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
   })
   .catch((err) => {
-    console.error(err);
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Nzg2NDU2MTJ8&ixlib=rb-4.1.0&q=80&w=1080)`;
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+
+    document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+  })
+  .catch((err) => console.error(err));
