7cc51e3cf28efc1888ad37e51457a7a013285e6c
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-14 13:06:15 +0800

    feat: add weather info from openweather API and setup chrome extension settings

--- src/main.js
+++ src/main.js
@@ -48,3 +48,27 @@ function renderTime() {
 }
 
 setInterval(renderTime, 1000);
+
+// get current weather with position info
+function getWeatherInfo() {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    try {
+      const res = await fetch(
+        `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`,
+      );
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      const data = await res.json();
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p>${Math.round(data.main.temp)}º</p>
+                <p>${data.name}</p>
+            `;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+getWeatherInfo();
